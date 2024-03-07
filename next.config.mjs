1ad544727f983cad82dbd047d8ac901f53f28338
R4NP3R <ssrafa.2014@gmail.com>
2024-03-07 12:57:30 -0300

    mudando a configuração do next.config.mjs para fazer o deploy na vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
     API_URL: "https://65e75d6b53d564627a8ea521.mockapi.io",
-    DOMAIN_ORIGIN: "http://localhost:3000",
+    DOMAIN_ORIGIN: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000",
   },
 };
 
