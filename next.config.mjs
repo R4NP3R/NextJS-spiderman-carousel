fe5f3df12b5b349f32f6c737593dfeaf43089e18
R4NP3R <ssrafa.2014@gmail.com>
2024-03-07 13:40:26 -0300

    mudando next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   env: {
     API_URL: "https://65e75d6b53d564627a8ea521.mockapi.io",
-    DOMAIN_ORIGIN: isProduction
+    DOMAIN_ORIGIN: process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:3000",
   },
 };
 
 export default nextConfig;
+
+console.log(process.env.NODE_ENV);
